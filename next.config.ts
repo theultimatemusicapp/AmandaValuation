6d2761df8d7d4e53a1c95f0d15a03a78323bb723
Carl <carl@music-records.com.au>
2026-01-17 04:00:24 +0700

    Add redirect for how-to-value-saas

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,16 @@ const nextConfig: NextConfig = {
         destination: '/',
         permanent: true,
       },
+      {
+        source: '/how-to-value-saas.html',
+        destination: '/resources/how-to-value-saas',
+        permanent: true,
+      },
+      {
+        source: '/how-to-value-saas',
+        destination: '/resources/how-to-value-saas',
+        permanent: true,
+      },
     ];
   },
 };
