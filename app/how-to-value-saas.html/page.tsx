164e1dc9831b7981dc6dff5336d84fa3a281feb7
Carl <carl@music-records.com.au>
2026-01-17 20:56:09 +0700

    Fix redirect for how-to-value SaaS resource

--- app/how-to-value-saas.html/page.tsx
+++ app/how-to-value-saas.html/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function HowToValueSaasHtmlRedirect() {
-    redirect('/resources/how-to-value-saas');
+    redirect('/resources/how-to-value-a-saas');
 }
